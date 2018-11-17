1c5440fabde86230be5c462f074c0a2a37b060e6
MarkZ2018 <41009850+MarkZ2018@users.noreply.github.com>
2018-11-17 06:34:49 -0500

    Add files via upload

---
+++ a3p2/king-of-new-york-obs-ver/Token.cpp
@@ -0,0 +1,56 @@
+#include "Token.h"
+Token::Token() :Token("", "", Token::Type::Web) {};
+Token::Token(string name, string description, Token::Type type) {
+	this->setName(name);
+	this->setDescription(description);
+	this->setType(type);
+}
+
+inline void Token::setName(string name) {
+	this->name = name;
+}
+
+inline void Token::setDescription(string description) {
+	this->description = description;
+}
+
+inline string Token::getName() const {
+	return this->name;
+}
+
+inline string Token::getDescription() const {
+	return this->description; 
+}
+
+inline void Token::setType(Token::Type type) {
+	this->type = type;
+}
+
+inline Token::Type Token::getType() const{
+	return this->type;
+}
+
+
+inline ostream& operator<<(ostream & strm, Token::Type const& type) {
+	switch (type) {
+	case Token::Type::Web:
+		return strm << "Web";
+		break;
+	case Token::Type::Carapace:
+		return strm << "Carapace";
+		break;
+	case Token::Type::Souvenir:
+		return strm << "Souvenir";
+		break;
+	case Token::Type::Jinx:
+		return strm << "Jinx";
+		break;
+	default:
+		return strm << "Not a TokenType";
+		break;
+	}
+}
+
+ostream& operator<<(ostream &strm, Token const &token) {
+	return strm << "Token(type:" << token.type << "name:" << token.name << ";description:" << token.description << ")";
+}
